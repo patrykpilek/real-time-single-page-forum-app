95658084c4f35c5e3d774c47112f8f3959875aa7
Patryk Pilek <patryk.pilek@gmail.com>
2018-05-08 01:42:39 +0100

    Factory and seed.

--- database/factories/LikesFactory.php
+++ database/factories/LikesFactory.php
@@ -1,9 +1,12 @@
 <?php
 
+use App\Model\User;
 use Faker\Generator as Faker;
 
 $factory->define(App\Model\Likes::class, function (Faker $faker) {
     return [
-        //
+        'user_id' => function() {
+            return User::all()->random();
+        }
     ];
 });
