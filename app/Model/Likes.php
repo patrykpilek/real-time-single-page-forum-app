4e6a00f14112269ac4f310e71fa7aa0e30b60e88
Patryk Pilek <patryk.pilek@gmail.com>
2018-05-21 12:38:30 +0100

    Like and Dislike function for reply.

--- app/Model/Likes.php
+++ app/Model/Likes.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Likes extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['reply_id', 'user_id'];
 }
